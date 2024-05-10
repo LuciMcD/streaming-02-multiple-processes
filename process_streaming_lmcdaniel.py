58a82146f916a0e2f6e54ee05fd4bd15eb2a96b7
LuciMcD <lucimcdaniel@gmail.com>
2024-05-10 12:30:56 -0500

    second try at Task 4

--- process_streaming_lmcdaniel.py
+++ process_streaming_lmcdaniel.py
@@ -39,7 +39,7 @@ HOST = "localhost"
 PORT = 9999
 ADDRESS_TUPLE = (HOST, PORT)
 INPUT_FILE_NAME = "all_month.csv"
-OUTPUT_FILE_NAME = "out9.txt"
+OUTPUT_FILE_NAME = "out9.csv"
 
 # Define program functions (bits of reusable code)
 
@@ -55,6 +55,34 @@ def prepare_message_from_row(row):
     MESSAGE = fstring_message.encode()
     logging.debug(f"Prepared message: {fstring_message}")
     return MESSAGE
+
+def process_rows(input_file_name, output_file_name):
+    logging.info(f"Calling process_rows(): {input_file_name} to {output_file_name}.")
+
+    # Create a file object for input (r = read access)
+    with open(input_file_name, "r") as input_file:
+        logging.info(f"Opened for reading: {input_file_name}.")
+
+        # Create a CSV reader object
+        reader = csv.reader(input_file, delimiter=",")
+
+        header = next(reader)  # Our file has a header row, move to next to get to data
+        logging.info(f"Skipped header row: {header}")
+
+        # Create a file object for output (w = write access)
+        # Set the newline parameter to an empty string to avoid extra newlines in the output file
+        with open(output_file_name, "w", newline="") as output_file:
+            logging.info(f"Opened for writing: {output_file_name}.")
+
+            # Create a CSV writer object
+            writer =csv.writer(output_file, delimiter=",")
+
+            # Write the header row to the output file
+            writer.writerow(["time", "latitude", "longitude", "depth", "mag", "magType", "nst", "gap", "dmin", "rms", "net", "id", "updated", "place", "type", "horizontalError", "depthError", "magError", "magNst", "status", "locationSource", "magSource"])
+
+            for row in reader:
+                time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource = row
+                writer.writerow({time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource}) 
 
 
 def stream_row(input_file_name, address_tuple):
@@ -88,35 +116,6 @@ def stream_row(input_file_name, address_tuple):
             sock_object.sendto(MESSAGE, address_tuple)
             logging.info(f"Sent: {MESSAGE} on port {PORT}. Hit CTRL-c to stop.")
             time.sleep(3) # wait 3 seconds between messages
-    
-def process_rows(input_file_name, output_file_name):
-    logging.info(f"Calling process_rows(): {input_file_name} to {output_file_name}.")
-
-    # Create a file object for input (r = read access)
-    with open(input_file_name, "r") as input_file:
-        logging.info(f"Opened for reading: {input_file_name}.")
-
-        # Create a CSV reader object
-        reader = csv.reader(input_file, delimiter=",")
-
-        header = next(reader)  # Our file has a header row, move to next to get to data
-        logging.info(f"Skipped header row: {header}")
-
-        # Create a file object for output (w = write access)
-        # Set the newline parameter to an empty string to avoid extra newlines in the output file
-        with open(output_file_name, "w", newline="") as output_file:
-            logging.info(f"Opened for writing: {output_file_name}.")
-
-            # Create a CSV writer object
-            writer = csv.writer(output_file, delimiter=",")
-
-            # Write the header row to the output file
-            writer.writerow(["time", "latitude", "longitude", "depth", "mag", "magType", "nst", "gap", "dmin", "rms", "net", "id", "updated", "place", "type", "horizontalError", "depthError", "magError", "magNst", "status", "locationSource", "magSource"])
-
-            for row in reader:
-                time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource = row
-                writer.writerow({time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource}) 
-     
 
 # ---------------------------------------------------------------------------
 # If this is the script we are running, then call some functions and execute code!
