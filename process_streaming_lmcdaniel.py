30d5df12b0df9e800242b416bed5aa7826189a6c
LuciMcD <lucimcdaniel@gmail.com>
2024-05-10 12:05:22 -0500

    first part of Task 4

--- process_streaming_lmcdaniel.py
+++ process_streaming_lmcdaniel.py
@@ -39,16 +39,17 @@ HOST = "localhost"
 PORT = 9999
 ADDRESS_TUPLE = (HOST, PORT)
 INPUT_FILE_NAME = "all_month.csv"
+OUTPUT_FILE_NAME = "out9.txt"
 
 # Define program functions (bits of reusable code)
 
 
 def prepare_message_from_row(row):
     """Prepare a binary message from a given row."""
-    time, latitude, longitude, depth, mag = row
+    time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource = row
     # use an fstring to create a message from our data
     # notice the f before the opening quote for our string?
-    fstring_message = f"[{time}, {latitude}, {longitude}, {depth}, {mag}]"
+    fstring_message = f"[{time}, {latitude}, {longitude}, {depth}, {mag}, {magType}, {nst}, {gap}, {dmin}, {rms}, {net}, {id}, {updated}, {place}, {type}, {horizontalError}, {depthError}, {magError}, {magNst}, {status}, {locationSource}, {magSource}]"
 
     # prepare a binary (1s and 0s) message to stream
     MESSAGE = fstring_message.encode()
@@ -61,7 +62,7 @@ def stream_row(input_file_name, address_tuple):
     logging.info(f"Starting to stream data from {input_file_name} to {address_tuple}.")
 
     # Create a file object for input (r = read access)
-    with open(input_file_name, "r") as input_file:
+    with open(input_file_name, "r", encoding="utf-8") as input_file:
         logging.info(f"Opened for reading: {input_file_name}.")
 
         # Create a CSV reader object
@@ -87,6 +88,35 @@ def stream_row(input_file_name, address_tuple):
             sock_object.sendto(MESSAGE, address_tuple)
             logging.info(f"Sent: {MESSAGE} on port {PORT}. Hit CTRL-c to stop.")
             time.sleep(3) # wait 3 seconds between messages
+    
+def process_rows(input_file_name, output_file_name):
+    logging.info(f"Calling process_rows(): {input_file_name} to {output_file_name}.")
+
+    # Create a file object for input (r = read access)
+    with open(input_file_name, "r") as input_file:
+        logging.info(f"Opened for reading: {input_file_name}.")
+
+        # Create a CSV reader object
+        reader = csv.reader(input_file, delimiter=",")
+
+        header = next(reader)  # Our file has a header row, move to next to get to data
+        logging.info(f"Skipped header row: {header}")
+
+        # Create a file object for output (w = write access)
+        # Set the newline parameter to an empty string to avoid extra newlines in the output file
+        with open(output_file_name, "w", newline="") as output_file:
+            logging.info(f"Opened for writing: {output_file_name}.")
+
+            # Create a CSV writer object
+            writer = csv.writer(output_file, delimiter=",")
+
+            # Write the header row to the output file
+            writer.writerow(["time", "latitude", "longitude", "depth", "mag", "magType", "nst", "gap", "dmin", "rms", "net", "id", "updated", "place", "type", "horizontalError", "depthError", "magError", "magNst", "status", "locationSource", "magSource"])
+
+            for row in reader:
+                time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource = row
+                writer.writerow({time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, id, updated, place, type, horizontalError, depthError, magError, magNst, status, locationSource, magSource}) 
+     
 
 # ---------------------------------------------------------------------------
 # If this is the script we are running, then call some functions and execute code!
@@ -98,6 +128,9 @@ if __name__ == "__main__":
         logging.info("Starting fake streaming process.")
         stream_row(INPUT_FILE_NAME, ADDRESS_TUPLE)
         logging.info("Streaming complete!")
+        logging.info("Start batch process")
+        process_rows(INPUT_FILE_NAME, OUTPUT_FILE_NAME)
+        logging.info("Process complete.")
         logging.info("===============================================")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
